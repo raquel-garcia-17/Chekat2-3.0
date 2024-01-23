0219e4bc336d9142a4122395252d60ec493cd697
Raquel García <63137137+raquel-garcia-17@users.noreply.github.com>
2024-01-23 13:15:52 -0600

    Add files via upload

---
+++ Handlers/CardviewD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChekaT2.Handlers
+{
+    public partial class CardView : ContentView
+    {
+        public static readonly BindableProperty CardTitleProperty = BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(CardView), string.Empty);
+
+        public string CardTitle
+        {
+            get => (string)GetValue(CardView.CardTitleProperty);
+            set => SetValue(CardView.CardTitleProperty, value);
+        }
+        // ...
+
+        public CardView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        //-    throw new ImplementedException();
+        }
+    }
+}
---
+++ Handlers/ClickLayout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChekaT2.Handlers
+{
+    public partial class ClickLayout 
+    {
+        public ClickLayout() { 
+        
+        }
+
+        void MenuItem(object sender, EventArgs args)
+        {
+            MenuItem item = new MenuItem {  };
+            item.Clicked += OnMenuItemClicked;
+        }
+
+        void OnMenuItemClicked(object sender, EventArgs args)
+        {
+            MenuItem menuItem = sender as MenuItem;
+
+            var contextItem = menuItem.BindingContext;
+        }
+    }
+}
